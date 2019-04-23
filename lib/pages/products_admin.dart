355b87beb89db8bb9cacd54ef1b7d6aafa2afefc
Gelson Rodrigues <GelsonMRodrigues@hotmail.com>
2019-04-23 09:09:27 -0300

    Added ProductsAdmin and the navigation to it

---
+++ lib/pages/products_admin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductsAdminPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Admin'),
+      ),
+      body: Center(
+        child: Text('This is the page'),
+      ),
+    );
+  }
+}
