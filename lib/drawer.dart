bb7b8a1e36b4cb863ac6b7c6418ed50c482c9083
Gelson Rodrigues <GelsonMRodrigues@hotmail.com>
2019-05-02 17:59:50 -0300

    Added Drawer to ProductsAdminPage

---
+++ lib/drawer.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import './pages/products_admin.dart';
+import './pages/products.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text('Choose'),
+          ),
+          ListTile(
+            title: Text('Home'),
+            onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => ProductsPage(),
+                )),
+          ),
+          ListTile(
+            title: Text('Manage Products'),
+            onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => ProductsAdminPage(),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import './../drawer.dart';
+
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: AppDrawer(),
       appBar: AppBar(
         title: Text('Manage Products'),
       ),
