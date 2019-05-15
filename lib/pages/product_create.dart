13aca28f34cef8e446bee527e85d3fc4a77f7e3f
Gelson Rodrigues <GelsonMRodrigues@hotmail.com>
2019-05-15 18:13:52 -0300

    Add labels to TextFields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -11,39 +11,45 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String titleValue = '';
   String descriptionValue = '';
   double priceValue = 0.0;
-  
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          onChanged: (String value) {
-            setState(() {
-              titleValue = value;
-            });
-          },
-        ),
-        Text('Title: ' + titleValue),
-        TextField(
-          keyboardType: TextInputType.multiline,
-          maxLines: 4,
-          onChanged: (String value) {
-            setState(() {
-              descriptionValue = value;
-            });
-          },
-        ),
-        Text('Description: ' + descriptionValue),
-        TextField(
-          keyboardType: TextInputType.number,
-          onChanged: (String value) {
-            setState(() {
-              priceValue = double.parse(value);
-            });
-          },
-        ),
-        Text('Price: ' + priceValue.toString()),
-      ],
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Product title'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          Text('Title: ' + titleValue),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product description'),
+            keyboardType: TextInputType.multiline,
+            maxLines: 4,
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          Text('Description: ' + descriptionValue),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product price'),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+          Text('Price: ' + priceValue.toString()),
+        ],
+      ),
     );
   }
 }
