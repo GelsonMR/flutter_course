1be5450b5044fccd52107a5faabab57fd033a369
Gelson Rodrigues <GelsonMRodrigues@hotmail.com>
2019-06-13 18:02:00 -0300

    Refactored private property names

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -13,9 +13,9 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String titleValue = '';
-  String descriptionValue = '';
-  double priceValue = 0.0;
+  String _titleValue = '';
+  String _descriptionValue = '';
+  double _priceValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -27,32 +27,32 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
             decoration: InputDecoration(labelText: 'Product title'),
             onChanged: (String value) {
               setState(() {
-                titleValue = value;
+                _titleValue = value;
               });
             },
           ),
-          Text('Title: ' + titleValue),
+          Text('Title: ' + _titleValue),
           TextField(
             decoration: InputDecoration(labelText: 'Product description'),
             keyboardType: TextInputType.multiline,
             maxLines: 4,
             onChanged: (String value) {
               setState(() {
-                descriptionValue = value;
+                _descriptionValue = value;
               });
             },
           ),
-          Text('Description: ' + descriptionValue),
+          Text('Description: ' + _descriptionValue),
           TextField(
             decoration: InputDecoration(labelText: 'Product price'),
             keyboardType: TextInputType.number,
             onChanged: (String value) {
               setState(() {
-                priceValue = double.parse(value);
+                _priceValue = double.parse(value);
               });
             },
           ),
-          Text('Price: ' + priceValue.toString()),
+          Text('Price: ' + _priceValue.toString()),
           SizedBox(height: 10.0,),
           RaisedButton(
             child: Text('Save'),
@@ -60,9 +60,9 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
             textColor: Colors.white,
             onPressed: () {
               final Map<String, dynamic> product = {
-                'title': titleValue,
-                'description': descriptionValue,
-                'price': priceValue,
+                'title': _titleValue,
+                'description': _descriptionValue,
+                'price': _priceValue,
                 'image': 'assets/food.jpg'
               };
               widget.addProduct(product);
